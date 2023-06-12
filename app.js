e20d0a0a49af652a2bd6436d1dfd2fb3faba5336
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-06-12 17:25:57 +0200

    - Fix eslint errors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function pageSwap() {
   let activeBtn = document.querySelector('.active-btn');
 
   for (const btn of sectBtn) {
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', function () {
       for (const b of sectBtn) {
         b.classList.remove('active-btn');
       }
@@ -17,21 +17,21 @@ function pageSwap() {
   }
 
   content.addEventListener('click', (e) => {
-     const id = e.target.dataset.id;
-     if(id) {
-        sectBtns.forEach((btn) => {
-            btn.classList.remove('active');
-        })
-        e.target.classList.add('active')
+    const { id } = e.target.dataset;
+    if (id) {
+      sectBtns.forEach((btn) => {
+        btn.classList.remove('active');
+      });
+      e.target.classList.add('active');
 
-        sections.forEach((section) => {
-            section.classList.remove('active')
-        })
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      });
 
-        const element = document.getElementById(id);
-        element.classList.add('active');
-     }
-  })
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
 }
 
 pageSwap();
