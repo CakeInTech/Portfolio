63c82f37b8c7844530eadae80302ba49c920b644
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-06-12 17:54:23 +0200

    - Fix styling erros for this

--- app.js
+++ app.js
@@ -4,27 +4,28 @@ const sectBtn = document.querySelectorAll('.control');
 const content = document.querySelector('.content');
 
 function pageSwap() {
-  let activeBtn = document.querySelector('.active-btn');
+  // eslint-disable-next-line no-unused-vars
+  let activeBtn;
 
-  for (const btn of sectBtn) {
+  sectBtn.forEach((btn) => {
     btn.addEventListener('click', function () {
-      for (const b of sectBtn) {
+      sectBtn.forEach((b) => {
         b.classList.remove('active-btn');
-      }
+      });
       this.classList.add('active-btn');
       activeBtn = this;
     });
-  }
+  });
 
   content.addEventListener('click', (e) => {
     const { id } = e.target.dataset;
     if (id) {
-      sectBtns.forEach((btn) => {
+      Array.prototype.forEach.call(sectBtns, (btn) => {
         btn.classList.remove('active');
       });
       e.target.classList.add('active');
 
-      sections.forEach((section) => {
+      Array.prototype.forEach.call(sections, (section) => {
         section.classList.remove('active');
       });
 
